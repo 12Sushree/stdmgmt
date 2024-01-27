2d01e4edc0a9a4a86315a109d5235859bcd006a4
Sushreeta Sahu <sushreeta.shu@gmail.com>
2024-01-27 07:29:10 +0000

    Connect to DB initiate

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express")
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+// Connecting to MongoDB
+connectDB();
 
 // Initiating the app
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 5000
+// Enabling CORS here for safety of access of info from other domains
+app.use(cors());
+// Handle JSON parsing properly
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-})
+});
