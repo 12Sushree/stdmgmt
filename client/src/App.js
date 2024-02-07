438e094a40a6cdd81ced5735bb4c93ce2f3d7991
Sushreeta Sahu <sushreeta.shu@gmail.com>
2024-02-07 05:28:41 +0000

    Add all routes in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import AddStudent from './components/AddStudent'
 import ShowStudentList from './components/ShowStudentList'
 import ShowStudentDetails from './components/ShowStudentDetails'
 import UpdateStudentInfo from './components/UpdateStudentInfo'
+import StudentCard from './components/StudentCard'
 
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
+          <Route exact path='/' element={<StudentCard />} />
           <Route path='/student-list' element={<ShowStudentList />} />
           <Route path='/add-student' element={<AddStudent />} />
           <Route path='/edit-student-info' element={<UpdateStudentInfo />} />
---
+++ client/src/components/UpdateStudentInfo.js
@@ -0,0 +1,18 @@
+// import React, { useState, useEffect } from 'react';
+//import { Link, useParams, useNavigate } from 'react-router-dom';
+//import axios from 'axios';
+//import '../App.css';
+
+function Updatestudentinfo(props) {
+    return (
+      <div className='Updatestudentinfo'>
+        <div className='container'>
+          <div className='row'>
+            <h1>This is Update Student Page</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Updatestudentinfo;
