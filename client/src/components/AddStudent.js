e9e6647463156309fb16dd9552730e44635d7fde
Sushreeta Sahu <sushreeta.shu@gmail.com>
2024-02-12 07:36:08 +0000

    Add react toaster, Connect to MongoDB

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Slide, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
@@ -8,26 +10,92 @@ import Footer from './Footer';
 
 const AddStudent = () => {
 
+  const navigate = useNavigate();
+  const [student, setStudent] = useState({
+    Name: '',
+    Id: '',
+    Honors: '',
+    Year: '',
+    DOB: '',
+    PhnNo: '',
+  });
+  const [showToast, setShowToast] = useState(false);
+
+  const onChange = (e) => {
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
+
   const onSubmit = (e) => {
     // Prevents the page from refreshing
     e.preventDefault()
 
-    // axios.post('api/students',student)
-    //   .then((res) => {
-    //     setStudent({
-    //       Name: '',
-    //       ID: '',
-    //       Honors: '',
-    //       Year: '',
-    //       DOB: '',
-    //       PhnNo: ''
-    //     })
-    //   })
+    axios.post('api/students',student)
+      .then((res) => {
+        setStudent({
+          Name: '',
+          ID: '',
+          Honors: '',
+          Year: '',
+          DOB: '',
+          PhnNo: ''
+        })
+
+      // Show the success alert
+      toast.success('Sudent added successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Slide,
+        });
+
+        // Delay the navigation slightly to allow the toast to be seen
+        setTimeout(() => {
+          setShowToast(false); // Hide the toast
+          navigate('/'); // Navigate to homepage
+        },  5000); // Adjust the timeout as needed
+
+      })
+
+      .catch((err) => {
+        console.log('Error in AddStudent!');
+        console.log('The error is -> ')
+        console.log(err)
+        // Show the success alert
+        toast.error('Something went wrong, try again!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Slide,
+          });
+      });
   }
 
   return (
     <div className='AddStudent'>
       <NavBar />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
       <div className='container'>
         <div className='row'>
           <div className='col-md-8 m-auto'>
@@ -48,8 +116,8 @@ const AddStudent = () => {
                   placeholder='Name of the Student'
                   name='name'
                   className='form-control'
-                  // value={Student.Name}
-                  // onChange={onChange}
+                  value={student.Name}
+                  onChange={onChange}
                 />
               </div>
 
@@ -59,8 +127,8 @@ const AddStudent = () => {
                   placeholder='Id of the Student'
                   name='id'
                   className='form-control'
-                  // value={Student.Name}
-                  // onChange={onChange}
+                  value={student.Name}
+                  onChange={onChange}
                 />
               </div>
 
@@ -70,8 +138,8 @@ const AddStudent = () => {
                   placeholder='Honors of the Student'
                   name='hons'
                   className='form-control'
-                  // value={Student.Name}
-                  // onChange={onChange}
+                  value={student.Name}
+                  onChange={onChange}
                 />
               </div>
 
@@ -81,10 +149,33 @@ const AddStudent = () => {
                   placeholder='Year of the Student'
                   name='yr'
                   className='form-control'
-                  // value={Student.Name}
-                  // onChange={onChange}
+                  value={student.Name}
+                  onChange={onChange}
                 />
               </div>
+
+              <div className='form-group'>
+                <input
+                  type='number'
+                  placeholder='DOB of the Student'
+                  name='dob'
+                  className='form-control'
+                  value={student.Name}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='number'
+                  placeholder='Phone No. of the Student'
+                  name='phn'
+                  className='form-control'
+                  value={student.Name}
+                  onChange={onChange}
+                />
+              </div>
+
             </form>
           </div>
         </div>
