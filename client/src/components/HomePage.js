cdd06493a904c0f9681016dceec710de81cede9a
Sushreeta Sahu <sushreeta.shu@gmail.com>
2024-02-16 12:46:52 +0000

    Add CSS Code

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -19,7 +19,6 @@ const Home = (props) => {
         </div>
       </div>
 
-      <Footer />
     </div>
   )
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,8 @@ import React from 'react'
 const NavBar = () => (
   <nav class="navbar navbar-expand-lg navbar-light text-dark h-auto nav">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Student Management System</a>
+      <img src="https://plus.unsplash.com/premium_photo-1681487975579-3cb90dbe46a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZWR1Y2F0aW9uJTIwaWNvbnN8ZW58MHx8MHx8fDA%3D" className='img-fluid logo' height="50" width="50" />
+      <a class="navbar-brand" href="#">Student Management</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -11,13 +12,19 @@ const NavBar = () => (
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link active" href="#home">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#features">Features</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#pricing">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
           </li>
         </ul>
       </div>
